0b7da433dbd9543aec2ed0adb3a662bf351231f3
Juan José Martinez Grisales <josemargri3@gmail.com>
2025-08-17 17:52:12 -0500

    feat: ✨ add new functionalities and simplify structure in navbar component

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,4 @@
-import { Button } from "@heroui/button";
-import { Kbd } from "@heroui/kbd";
 import { Link } from "@heroui/link";
-import { Input } from "@heroui/input";
 import {
   Navbar as HeroUINavbar,
   NavbarBrand,
@@ -11,132 +8,139 @@ import {
   NavbarMenu,
   NavbarMenuItem,
 } from "@heroui/navbar";
-import { link as linkStyles } from "@heroui/theme";
-import clsx from "clsx";
+import {
+  DropdownItem,
+  DropdownTrigger,
+  Dropdown,
+  DropdownMenu,
+  Avatar,
+  Button,
+} from "@heroui/react";
+import { useState } from "react";
 
 import { siteConfig } from "@/config/site";
-import { ThemeSwitch } from "@/components/theme-switch";
-import {
-  TwitterIcon,
-  GithubIcon,
-  DiscordIcon,
-  HeartFilledIcon,
-  SearchIcon,
-} from "@/components/icons";
 import { Logo } from "@/components/icons";
 
+//TODO: Modificar la logica para los estados globales reales y links reales
 export const Navbar = () => {
-  const searchInput = (
-    <Input
-      aria-label="Search"
-      classNames={{
-        inputWrapper: "bg-default-100",
-        input: "text-sm",
-      }}
-      endContent={
-        <Kbd className="hidden lg:inline-block" keys={["command"]}>
-          K
-        </Kbd>
-      }
-      labelPlacement="outside"
-      placeholder="Search..."
-      startContent={
-        <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-      }
-      type="search"
-    />
-  );
+  const [isAuth] = useState(false);
 
   return (
-    <HeroUINavbar maxWidth="xl" position="sticky">
-      <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
-        <NavbarBrand className="gap-3 max-w-fit">
-          <Link
-            className="flex justify-start items-center gap-1"
-            color="foreground"
-            href="/"
-          >
-            <Logo />
-            <p className="font-bold text-inherit">ACME</p>
-          </Link>
-        </NavbarBrand>
-        <div className="hidden lg:flex gap-4 justify-start ml-2">
-          {siteConfig.navItems.map((item) => (
-            <NavbarItem key={item.href}>
-              <Link
-                className={clsx(
-                  linkStyles({ color: "foreground" }),
-                  "data-[active=true]:text-primary data-[active=true]:font-medium"
-                )}
-                color="foreground"
-                href={item.href}
-              >
-                {item.label}
-              </Link>
-            </NavbarItem>
-          ))}
-        </div>
-      </NavbarContent>
+    <HeroUINavbar maxWidth="2xl" position="sticky">
+      <NavbarBrand className="gap-3 max-w-fit">
+        <Link
+          className="flex justify-start items-center gap-1"
+          color="foreground"
+          href="/"
+        >
+          <Logo />
+          <p className="font-bold text-inherit">SIATA</p>
+        </Link>
+      </NavbarBrand>
+      {isAuth && (
+        <NavbarContent className="basis-1/5 sm:basis-full" justify="center">
+          <div className="hidden lg:flex gap-4 justify-start ml-2">
+            {siteConfig.privateNavItems.map((item) => (
+              <NavbarItem key={item.href}>
+                <Link color="foreground" href={item.href}>
+                  {item.label}
+                </Link>
+              </NavbarItem>
+            ))}
+          </div>
+        </NavbarContent>
+      )}
 
-      <NavbarContent
-        className="hidden sm:flex basis-1/5 sm:basis-full"
-        justify="end"
-      >
-        <NavbarItem className="hidden sm:flex gap-2">
-          <Link isExternal href={siteConfig.links.twitter} title="Twitter">
-            <TwitterIcon className="text-default-500" />
-          </Link>
-          <Link isExternal href={siteConfig.links.discord} title="Discord">
-            <DiscordIcon className="text-default-500" />
-          </Link>
-          <Link isExternal href={siteConfig.links.github} title="GitHub">
-            <GithubIcon className="text-default-500" />
-          </Link>
-          <ThemeSwitch />
-        </NavbarItem>
-        <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
-        <NavbarItem className="hidden md:flex">
-          <Button
-            isExternal
-            as={Link}
-            className="text-sm font-normal text-default-600 bg-default-100"
-            href={siteConfig.links.sponsor}
-            startContent={<HeartFilledIcon className="text-danger" />}
-            variant="flat"
-          >
-            Login
-          </Button>
-        </NavbarItem>
+      <NavbarContent as="div" justify="end">
+        {isAuth ? (
+          <Dropdown placement="bottom-end">
+            <DropdownTrigger>
+              <Avatar
+                isBordered
+                as="button"
+                className="transition-transform"
+                color="secondary"
+                name="Jason Hughes"
+                size="sm"
+                src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+              />
+            </DropdownTrigger>
+            <DropdownMenu aria-label="Profile Actions" variant="flat">
+              <DropdownItem key="profile" className="h-14 gap-2">
+                <p className="font-semibold">Sesion iniciada con</p>
+                <p className="font-semibold">zoey@example.com</p>
+              </DropdownItem>
+              <DropdownItem key="logout" color="danger">
+                Cerrar Sesión
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        ) : (
+          <>
+            <NavbarItem className="hidden lg:flex">
+              <Link href="#">Iniciar Sesión</Link>
+            </NavbarItem>
+            <NavbarItem>
+              <Button
+                className="hidden lg:flex"
+                color="primary"
+                href="#"
+                variant="flat"
+              >
+                Registrarse
+              </Button>
+            </NavbarItem>
+          </>
+        )}
       </NavbarContent>
 
       <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-        <Link isExternal href={siteConfig.links.github}>
-          <GithubIcon className="text-default-500" />
-        </Link>
-        <ThemeSwitch />
         <NavbarMenuToggle />
       </NavbarContent>
 
       <NavbarMenu>
-        {searchInput}
         <div className="mx-4 mt-2 flex flex-col gap-2">
-          {siteConfig.navMenuItems.map((item, index) => (
-            <NavbarMenuItem key={`${item}-${index}`}>
-              <Link
-                color={
-                  index === 2
-                    ? "primary"
-                    : index === siteConfig.navMenuItems.length - 1
-                      ? "danger"
-                      : "foreground"
-                }
-                href="#"
-                size="lg"
-              >
-                {item.label}
+          {isAuth &&
+            siteConfig.privateNavItems.map((item, index) => (
+              <NavbarMenuItem key={`${item.href}-${index}`}>
+                <Link color="foreground" href={item.href} size="lg">
+                  {item.label}
+                </Link>
+              </NavbarMenuItem>
+            ))}
+
+          {isAuth && (
+            <>
+              <hr className="my-2 border-gray-300" />
+              <div className="flex flex-col gap-1">
+                <p className="text-sm font-semibold">Signed in as</p>
+                <p className="text-sm text-default-500">zoey@example.com</p>
+                <Button className="mt-2 w-fit" color="danger" variant="flat">
+                  Cerrar Sesión
+                </Button>
+              </div>
+            </>
+          )}
+          {isAuth ? (
+            <NavbarMenuItem>
+              <Link color="foreground" href="" size="lg">
+                Cerrar Sesión
               </Link>
             </NavbarMenuItem>
-          ))}
+          ) : (
+            <>
+              <NavbarMenuItem>
+                <Link color="foreground" href="" size="lg">
+                  Iniciar Sesión
+                </Link>
+              </NavbarMenuItem>
+              <NavbarMenuItem>
+                <Link color="foreground" href="" size="lg">
+                  Registrarse
+                </Link>
+              </NavbarMenuItem>
+            </>
+          )}
         </div>
       </NavbarMenu>
     </HeroUINavbar>
--- src/config/site.ts
+++ src/config/site.ts
@@ -3,60 +3,14 @@ export type SiteConfig = typeof siteConfig;
 export const siteConfig = {
   name: "Vite + HeroUI",
   description: "Make beautiful websites regardless of your design experience.",
-  navItems: [
+  privateNavItems: [
     {
-      label: "Home",
-      href: "/",
+      label: "Estaciones",
+      href: "/estaciones",
     },
     {
-      label: "Docs",
-      href: "/docs",
-    },
-    {
-      label: "Pricing",
-      href: "/pricing",
-    },
-    {
-      label: "Blog",
-      href: "/blog",
-    },
-    {
-      label: "About",
-      href: "/about",
-    },
-  ],
-  navMenuItems: [
-    {
-      label: "Profile",
-      href: "/profile",
-    },
-    {
-      label: "Dashboard",
-      href: "/dashboard",
-    },
-    {
-      label: "Projects",
-      href: "/projects",
-    },
-    {
-      label: "Team",
-      href: "/team",
-    },
-    {
-      label: "Calendar",
-      href: "/calendar",
-    },
-    {
-      label: "Settings",
-      href: "/settings",
-    },
-    {
-      label: "Help & Feedback",
-      href: "/help-feedback",
-    },
-    {
-      label: "Logout",
-      href: "/logout",
+      label: "Perfil",
+      href: "/perfil",
     },
   ],
   links: {
