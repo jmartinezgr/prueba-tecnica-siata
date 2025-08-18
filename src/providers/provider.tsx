c156082f9900aaa99ff42e53da8dbf1ed9be24e7
Juan José Martinez Grisales <josemargri3@gmail.com>
2025-08-18 04:08:27 -0500

    feat: ✨  configure React Query provider alongside existing app providers

--- src/providers/provider.tsx
+++ src/providers/provider.tsx
@@ -2,6 +2,7 @@ import type { NavigateOptions } from "react-router-dom";
 
 import { HeroUIProvider } from "@heroui/system";
 import { useHref, useNavigate } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { AuthenticationProvider } from "./auth/AuthProvider";
 
@@ -11,12 +12,18 @@ declare module "@react-types/shared" {
   }
 }
 
+const queryClient = new QueryClient();
+
 export function Provider({ children }: { children: React.ReactNode }) {
   const navigate = useNavigate();
 
   return (
-    <HeroUIProvider navigate={navigate} useHref={useHref}>
-      <AuthenticationProvider>{children}</AuthenticationProvider>
-    </HeroUIProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthenticationProvider>
+        <HeroUIProvider navigate={navigate} useHref={useHref}>
+          {children}
+        </HeroUIProvider>
+      </AuthenticationProvider>
+    </QueryClientProvider>
   );
 }
