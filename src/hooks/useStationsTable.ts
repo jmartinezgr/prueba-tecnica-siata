cd7bb6f6e22b62adbe7d23bd471f3547e61a0cce
Juan José Martinez Grisales <josemargri3@gmail.com>
2025-08-18 16:22:05 -0500

    feat: ✨ create and use a custom hook to handle all  of the dashboard logic

---
+++ src/hooks/useStationsTable.ts
@@ -0,0 +1,49 @@
+// hooks/useStationsTable.ts
+import { useMemo, useState } from "react";
+
+import { Station } from "@/types/stations";
+
+export function useStationsTable(stations: Station[]) {
+  const [filterValue, setFilterValue] = useState("");
+  const [statusFilter, setStatusFilter] = useState<string>("all");
+  const [page, setPage] = useState(1);
+  const rowsPerPage = 10;
+
+  const filteredItems = useMemo(() => {
+    let filtered = [...stations];
+
+    if (filterValue) {
+      filtered = filtered.filter(
+        (s) =>
+          s.name.toLowerCase().includes(filterValue.toLowerCase()) ||
+          s.location.toLowerCase().includes(filterValue.toLowerCase()) ||
+          s.type.toLowerCase().includes(filterValue.toLowerCase())
+      );
+    }
+    if (statusFilter !== "all") {
+      filtered = filtered.filter((s) => s.status === statusFilter);
+    }
+
+    return filtered;
+  }, [stations, filterValue, statusFilter]);
+
+  const pages = Math.ceil(filteredItems.length / rowsPerPage);
+
+  const items = useMemo(() => {
+    const start = (page - 1) * rowsPerPage;
+
+    return filteredItems.slice(start, start + rowsPerPage);
+  }, [page, filteredItems]);
+
+  return {
+    filterValue,
+    setFilterValue,
+    statusFilter,
+    setStatusFilter,
+    page,
+    setPage,
+    pages,
+    filteredItems,
+    items,
+  };
+}
