ac8941b5adeda3cfbe753065b857d00af02dafe7
Juan José Martinez Grisales <josemargri3@gmail.com>
2025-08-18 23:59:56 -0500

    feat: ✨ config the provider to use toast messages

--- src/providers/provider.tsx
+++ src/providers/provider.tsx
@@ -3,6 +3,7 @@ import type { NavigateOptions } from "react-router-dom";
 import { HeroUIProvider } from "@heroui/system";
 import { useHref, useLocation, useNavigate } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ToastProvider } from "@heroui/react";
 
 import { AuthenticationProvider } from "./auth/AuthProvider";
 
@@ -25,6 +26,7 @@ export function Provider({ children }: { children: React.ReactNode }) {
       <AuthenticationProvider navigate={navigate} pathname={pathname}>
         <AuthGuard navigate={navigate} pathname={pathname}>
           <HeroUIProvider navigate={navigate} useHref={useHref}>
+            <ToastProvider />
             {children}
           </HeroUIProvider>
         </AuthGuard>
