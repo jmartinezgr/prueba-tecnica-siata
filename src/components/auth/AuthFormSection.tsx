63b0619b16790c48cc9a467bc0fd111a56d3b979
Juan José Martinez Grisales <josemargri3@gmail.com>
2025-08-19 07:33:49 -0500

    feat: ✨ implaements a way to go "/" from auth pages

--- src/components/auth/AuthFormSection.tsx
+++ src/components/auth/AuthFormSection.tsx
@@ -1,5 +1,6 @@
-import { Card, CardBody } from "@heroui/react";
+import { Card, CardBody, Button } from "@heroui/react";
 import { Link } from "react-router-dom";
+import { IconHome } from "@tabler/icons-react";
 
 import { AuthFormSectionProps } from "@/types/auth";
 
@@ -9,11 +10,17 @@ const AuthFormSection = ({
   children,
 }: AuthFormSectionProps) => {
   return (
-    <div className="w-full lg:w-1/2 flex items-center justify-center p-8">
-      <div className="w-full max-w-md">
+    <div className="w-full lg:w-1/2 flex items-center justify-center p-8 relative">
+      {/* Botón flotante solo en mobile */}
+      <Link className="sm:hidden fixed top-4 left-4 z-50" to="/">
+        <Button className="p-2 rounded-full shadow" size="sm" variant="light">
+          <IconHome className="w-5 h-5" />
+        </Button>
+      </Link>
+
+      <div className="w-full max-w-md mt-3 sm:mt-0">
         <Card className="shadow-lg border border-slate-200">
           <CardBody className="p-6">
-            {" "}
             <div className="text-center mb-6">
               <h2 className="text-2xl font-semibold text-slate-800 mb-2">
                 {title}
@@ -23,6 +30,7 @@ const AuthFormSection = ({
             <div className="space-y-4">{children}</div>
           </CardBody>
         </Card>
+
         <div className="mt-6 text-center space-y-2">
           <p className="text-xs text-slate-500">
             © 2025 Valle de Aburra - Secretaría del Medio Ambiente
